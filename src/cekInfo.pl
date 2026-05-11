11c5d49391633aaaddbc8cbb7b7da9d147a714e0
Raya Medina <rayamedi1106@gmail.com>
2026-05-12 01:52:55 +0700

    fix: fix mainkanKartu.pl and fix cekInfo.pl

--- src/cekInfo.pl
+++ src/cekInfo.pl
@@ -1,3 +1,21 @@
+:- dynamic(game_started/0).
+:- dynamic(kartuTop/2). /* ambil kartu teratas */
+:- dynamic(kartu_diTangan/2).
+:- dynamic(giliran/1). /* mengambil giliran pemain*/
+
+info_pemain([]).
+info_pemain([Nama|Tail]) :-
+    (kartu_diTangan(Nama, Isi_kartu)
+    -> length(Isi_kartu, Jum)
+    ;
+    Jum = 0
+    ),
+    format('- ~w: ~d kartu ~n', [Nama, Jum]),
+    info_pemain(T).
+
 cekInfo :-
-write('Kartu discard-top: ~s~n', [discardtop]).
-write('Urutan pemain: ~s~n', [urutan_pemain]).
+kartuTop(Warna, jenis).
+write('Kartu discard-top: ~w~n', [Warna, Jenis]).
+write('Urutan pemain: ~w~n', [Urutan_pemain]).
+info_pemain(Urutan_pemain)
+
