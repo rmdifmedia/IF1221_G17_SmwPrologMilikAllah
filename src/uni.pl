24eb351218bb2732b8cffb85d14b09f1e8f580ca
Cendra Ayam <cendra.asih.c@gmail.com>
2026-05-19 17:11:00 +0700

    fix: fixed uni.pl (read code carefully for success run)

--- src/uni.pl
+++ src/uni.pl
@@ -1,12 +1,14 @@
-:- include('fact.pl').
 :- include('player.pl').
-:- include('helper.pl').
+:- include('fact.pl').
 
+/* KONDISI SUCCESS UNTUK TEST RUN INDIVIDU UNI.PL:
+- fact.pl diberi inilization(initdeck).
+- fact.pl include helper.pl saja
+- include uni.pl seperti tertera */
+
+:- dynamic(listUni/1).
+:- dynamic(kartu_diTangan/2).
 :- dynamic(updateListUni/0).
-
-
-:- initialization(write('hewwp')).
-
 
 turn(sawit).
 kartu_diTangan(sawit, [kartu(merah, 0), kartu(kuning, 1)]).
@@ -14,6 +16,11 @@ kartu_diTangan(sawit, [kartu(merah, 0), kartu(kuning, 1)]).
 /* List UNI */
 listUni([]).
 
+addListUni(Nama):-
+    retract(listUni(ListLama)),
+    assertz(listUni([Nama|ListLama])).
+
+/* Update List Uni -- tiap turn, karena Pemain bisa jadi kena wild four shg kartu > 1 */
 filter_cardIsOne([], []).
 
 filter_cardIsOne([H|T], [H|TResult]):-
@@ -28,21 +35,11 @@ filter_cardIsOne([H|T], TResult):-
     \+LenKartu =:= 1,
     filter_cardIsOne(T, TResult).
 
-addListUni(Nama):-
-    retract(listUni(ListLama)),
-    assertz(listUni([Nama|ListLama])).
-
-syarat_UNI(Nama):-
-    kartu_diTangan(Nama, ListKartu),
-    list_length(ListKartu, Len),
-    Len =:= 1.
-
 updateListUni:-
     listUni(ListLama),
     filter_cardIsOne(ListLama, ListBaru),
     retract(listUni(ListLama)),
     assertz(listUni(ListBaru)).
-
 
 /* Helper UNI */
 uni_Info(Pemain, ListKartu):-
@@ -59,6 +56,19 @@ uni(Number):-
     read(NewNumber),
     uni(NewNumber).
 
+uni(Number):-
+    uni_Info(Pemain, ListKartu),
+    list_length(ListKartu, LenKartu),
+    \+LenKartu =:= 2,
+    !,
+    write(Pemain), write(' tidak memenuhi syarat perintah UNI!'), nl,
+    repeat_N_ambilKartu(1, AmbilKartu),
+    write(Pemain), write(' mendapatkan 1 kartu acak.'), nl,
+    nextTurn.
+
+/* NOTES: repeat_N_ambilKartu gagal. Pending benerin fact.pl ke yang work di gnu prolog */
+/* SOLUSI: karena dummy, sementara fact pakai dikasih inilization initdeck. Selain buat ngecek uni sendiri, inilization di fact, hapus. */
+
 /* # Sementara dimatiin sampai efek kartu berhasil diimplikasi #
 uni(Number):-
     uni_Info(Pemain, ListKartu),
@@ -73,16 +83,6 @@ uni(Number):-
 uni(Number):-
     uni_Info(Pemain, ListKartu),
     list_length(ListKartu, LenKartu),
-    \+ LenKartu =:= 2,
-    !,
-    write(Pemain), write(' tidak memenuhi syarat perintah UNI!'), nl,
-    repeat_N_ambilKartu(1, AmbilKartu),
-    write(Pemain), write(' mendapatkan 1 kartu acak.'), nl,
-    nextTurn.
-
-uni(Number):-
-    uni_Info(Pemain, ListKartu),
-    list_length(ListKartu, LenKartu),
     (Number > 0, (Number < LenKartu ; Number == LenKartu)),
     /* # Sementara dimatiin sampai efek kartu berhasil diimplikasi # */
     /* Number1 is Number - 1,
@@ -90,6 +90,8 @@ uni(Number):-
     isKartuValid(Kartu), */
     LenKartu =:= 2,
     !,
-    mainkanKartu(Number),
+    /* mainkanKartu(Number), # Sementara dimatiin sampai main dari awal #*/  
     write(Pemain), write(' menyerukan UNI!'), nl,
     addListUni(Pemain).
+
+/* NOTES: Walaupun uni berhasil, ga akan substract kartu_diTangan UNTUK SAAT INI, karena rules mainkanKartu dimatikan*/
