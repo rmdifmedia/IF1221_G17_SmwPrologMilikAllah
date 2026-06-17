cba2d64b0cb10e9f7263c03b98af43994d4b05a3
ccpurceria <cherinette.corsane@gmail.com>
2026-06-17 20:43:54 +0700

    fix saveGame

--- src/saveGame.pl
+++ src/saveGame.pl
@@ -2,7 +2,6 @@
 :- include('fact.pl').
 :- include('startGame.pl').
 :- include('helper.pl').
-:- dynamic(listHistoryTop/1).
 
 inputNamaFile(NamaFileTxt) :-
     write('Masukkan nama file permainan: '),
@@ -15,54 +14,73 @@ insert_tail([], Tail, Tail).
 insert_tail([H|T], Tail, [H|Result]) :-
     insert_tail(T, Tail, Result).
 
-cekListHitory(_, [], hitam, wild):-!.
-cekListHitory(N, [W-J|Tail], W, J):-
-    isKartuAksi(W, J), !,
-    format('Kartu aksi yang terakhir dimainkan: ~w-~w (~w giliran lalu)', [W, J, N]), nl.
-cekListHitory(N, [W-J|Tail], HistoryW, HistoryJ):-
-    N1 is N + 1,
-    cekListHitory(N1, Tail, HistoryW, HistoryJ).
-
 saveGame :-
     inputNamaFile(NamaFileTxt),
     open(NamaFileTxt, write, Stream),
 
+    modeGame(Mode),
+    format(Stream, 'mode_game:~w.~n', [Mode]),
+
+    playerCount(Count),
+    format(Stream, 'player_count:~w.~n', [Count]),
+
     listUrutan(Urutan),
-    format(Stream, 'urutan_pemain: ~w.~n', [Urutan]),
+    format(Stream, 'urutan_pemain: ~q.~n', [Urutan]),
 
     turn(Giliran),
-    format(Stream, 'giliran: ~w.~n', [Giliran]),
+    format(Stream, 'giliran: ~q.~n', [Giliran]),
 
     kartuTop(W, J),
-    format(Stream, 'discard_top: ~w-~w.~n', [W, J]),
+    format(Stream, 'discard_top: ~q-~q.~n', [W, J]),
 
     currColor(Warna),
-    format(Stream, 'warna_aktif: ~w.~n', [Warna]),
+    format(Stream, 'warna_aktif: ~q.~n', [Warna]),
 
     arah(Arah),
-    format(Stream, 'arah_permainan: ~w.~n', [Arah]),
+    format(Stream, 'arah_permainan: ~q.~n', [Arah]),
 
     listUni(UNI),
-    format(Stream, 'status_UNI: ~w.~n', [UNI]),
-
-    listUrutan(Players),
-    saveKartuTangan(Players, Stream),
+    format(Stream, 'status_UNI: ~q.~n', [UNI]),
 
     listHistoryTop(History),
-    cekListHitory(1, History, HW, HJ),
-    format(Stream, 'Kartu_Aksi_Terakhir: ~w-~w.~n', [HW, HJ]),
+    format(Stream, 'list_history_top:~q.~n', [History]),
+
+    ( Mode == 2
+    -> savePlayerTeam(Urutan, Stream)
+    ; true
+    ),
+
+    saveKartuTangan(Urutan, Stream),
 
     close(Stream),
     format('Status permainan berhasil disimpan ke ~w.~n', [NamaFileTxt]).
 
+savePlayerTeam([], _).
+savePlayerTeam([Player|Rest], Stream) :-
+    playerTeamNumber(Player, TeamNum),
+    format(Stream, 'player_team:~q-~w.~n', [Player, TeamNum]),
+    savePlayerTeam(Rest, Stream).
+
 saveKartuTangan([], _).
 saveKartuTangan([Player|Rest], Stream) :-
     kartu_diTangan(Player, Kartu),
-    format(Stream, 'kartu(~w):~w.~n', [Player, Kartu]),
+    format(Stream, 'kartu(~q):~q.~n', [Player, Kartu]),
     saveKartuTangan(Rest, Stream).
 
 loadGame :-
     inputNamaFile(NamaFileTxt),
+    retractall(modeGame(_)),
+    retractall(playerCount(_)),
+    retractall(listUrutan(_)),
+    retractall(listPlayer(_)),
+    retractall(turn(_)),
+    retractall(kartuTop(_,_)),
+    retractall(currColor(_)),
+    retractall(arah(_)),
+    retractall(listUni(_)),
+    retractall(listHistoryTop(_)),
+    retractall(playerTeamNumber(_,_)),
+    retractall(kartu_diTangan(_,_)),
     open(NamaFileTxt, read, Stream),
     loadFakta(Stream),
     close(Stream),
@@ -76,12 +94,15 @@ loadFakta(NamaFile) :-
     ; restore_term(Term),
     loadFakta(NamaFile)
     ).
-
-restore_term(urutan_pemain:Urutan)   :- assertz(listUrutan(Urutan)).
+    
+restore_term(mode_game:Mode)         :- assertz(modeGame(Mode)).
+restore_term(player_count:Count)     :- assertz(playerCount(Count)).
+restore_term(urutan_pemain:Urutan)   :- assertz(listUrutan(Urutan)), assertz(listPlayer(Urutan)).
 restore_term(giliran:Giliran)        :- assertz(turn(Giliran)).
 restore_term(discard_top:W-J)        :- assertz(kartuTop(W, J)).
 restore_term(warna_aktif:Warna)      :- assertz(currColor(Warna)).
 restore_term(arah_permainan:Arah)    :- assertz(arah(Arah)).
 restore_term(status_UNI:UNI)         :- assertz(listUni(UNI)).
-restore_term(Kartu_Aksi_Terakhir:HW-HJ):- assertz(listHistoryTop(HW-HJ)).
+restore_term(list_history_top:History)   :- assertz(listHistoryTop(History)).
+restore_term(player_team:Player-TeamNum) :- assertz(playerTeamNumber(Player, TeamNum)).
 restore_term(kartu(Player):Kartu)    :- assertz(kartu_diTangan(Player, Kartu)).
